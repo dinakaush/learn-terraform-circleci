6784bef68f47af9da6e793a1912d61ae72312009
Dinithi Amarasinghe <95396388+dinakaush@users.noreply.github.com>
2024-02-07 15:53:40 +0530

    Update  main file

--- main.tf
+++ main.tf
@@ -36,15 +36,6 @@ resource "aws_s3_bucket_public_access_block" "app" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "app" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.app,
-    aws_s3_bucket_public_access_block.app,
-  ]
-
-  bucket = aws_s3_bucket.app.id
-  acl    = "public-read"
-}
 
 resource "aws_s3_bucket_website_configuration" "app" {
   bucket = aws_s3_bucket.app.bucket
